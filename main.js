bdaf5719faace4b7468b1973c59563451b5fbc61
Maxim Korinets <mkorinets@gmail.com>
2018-01-16 18:36:13 +0700

    Add tv device

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var zetta = require('zetta');
+var TV = require('./tv.js');
 
 
 zetta()
     .name('Zero Hub')
+    .use(TV)
     .listen(1337, function () {
         console.log('Zetta is running at http://127.0.0.1:1337');
 });
---
+++ tv.js
@@ -0,0 +1,33 @@
+var Device = require('zetta').Device;
+var util = require('util');
+
+
+var TV = module.exports = function() {
+    Device.call(this);
+};
+util.inherits(TV, Device);
+
+TV.prototype.init = function(config) {
+
+    config
+        .type('state_machine')
+        .state('off')
+        .name("TV");
+
+    config
+        .when('off', {allow: ['turn-on']})
+        .when('on', {allow: ['turn-off']})
+
+        .map('turn-off', this.turnOff)
+        .map('turn-on', this.turnOn)
+};
+
+TV.prototype.turnOff = function(done) {
+    this.state = 'off';
+    done();
+};
+
+TV.prototype.turnOn = function(done) {
+    this.state = 'on';
+    done();
+};
