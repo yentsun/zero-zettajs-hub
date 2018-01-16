c91524d5edf26ccb1093b0238b288c923d6d863b
Maxim Korinets <mkorinets@gmail.com>
2018-01-16 15:24:30 +0700

    Add pm2 and flightplan configs

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var zetta = require('zetta');
 
 zetta()
     .name('Zero Hub')
-    .link('http://hello-zetta.herokuapp.com/')
     .listen(1337, function () {
         console.log('Zetta is running at http://127.0.0.1:1337');
 });
